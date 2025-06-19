130994daec977d923bfdb7e831f3fd2af69f21a8
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-06-19 09:28:33 +0200

    0t-

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+"""0. Hello Flask!"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def f():
+    """0. Hello Flask!"""
     return "Hello HBNB!"
