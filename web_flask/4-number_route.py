7acc76e71df3a38c8bc4649ec17a28f01d1c543a
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-06-19 10:21:10 +0200

    4t

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""3. Python is cool!"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    return ("Hello HBNB!")
+
+@app.route('/hbnb', strict_slashes=False)
+def hb():
+    return ("HBNB")
+
+@app.route('/c/<text>', strict_slashes=False)
+def ct(text):
+    return f"C {text.replace('_', ' ')}"
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pt(text="is cool"):
+    return f"Python {text.replace('_', ' ')}"
+
+@app.route('/number/<int:n>')
+def nm(n):
+    return f"{n} is a number"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
